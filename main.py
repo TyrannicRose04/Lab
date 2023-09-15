c7a5f2aba1226b8bbbda72dca05ad4126b3b863f
TyrannicRose04 <TyrannicRose04@gmail.com>
2023-09-15 23:57:24 +0300

    Реализован парсинг курса доллара с сайта cbr за промежуток времени, вывод в файл csv.Также обработана ошибка, когда на определённый день нет статистики.

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import requests
 import datetime
 
-url = ('https://www.cbr-xml-daily.ru/archive/2022/09/08/daily_json.js')
-response = requests.get(url).json()
-date = response['Date']
-USD = response['Valute']['USD']['Value']
-with open('dataset.csv', 'a') as f:
-    f.write(date)
-    f.write(", ")
-    f.write(str(USD))
-    f.write("\n")
+d = datetime.date(2021, 9, 1)
+df = datetime.datetime.now() + datetime.timedelta(days=1)
+while d.year != df.year or d.month != df.month or d.day != df.day:
+    month = d.month
+    day = d.day
+    if month < 10:
+        month = "0" + str(d.month)
+    if day < 10:
+        day = "0" + str(d.day)
+    url = ('https://www.cbr-xml-daily.ru/archive/' + str(d.year) + '/' + str(month) + '/' + str(day) + '/daily_json.js')
+    response = requests.get(url).json()
+    if 'error' in response:
+        d += datetime.timedelta(days=1)
+    else:
+        date = response['Date']
+        USD = response['Valute']['USD']['Value']
+
+        with open('dataset.csv', 'a') as f:
+            f.write(date)
+            f.write(", ")
+            f.write(str(USD))
+            f.write("\n")
+        d += datetime.timedelta(days=1)
